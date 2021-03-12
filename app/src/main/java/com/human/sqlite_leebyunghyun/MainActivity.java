092a859d4048eb314b1a978367fae5619f2cd16d
domgenes@gmail.com <domgenes@gmail.com>
2021-03-12 11:49:20 +0900

    3교시 커밋

--- app/src/main/java/com/human/sqlite_leebyunghyun/MainActivity.java
+++ app/src/main/java/com/human/sqlite_leebyunghyun/MainActivity.java
@@ -2,13 +2,22 @@ package com.human.sqlite_leebyunghyun;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    //현재클래스에서 사용할 멤버변수 생성(아래)
+    private DatabaseHelper mDatabaseHelper;
+    private SQLiteDatabase mSqLiteDatabase;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //신규 데이터베이스 객체 생성=메모리에 올리기=실행가능하게 만들기(아래)
+        mDatabaseHelper = new DatabaseHelper(this,"school.db",null,1);
+        //데이터베이스 파일 만들기(아래)
+        mSqLiteDatabase = mDatabaseHelper.getWritableDatabase();
+        //테스트로 mSqliteDatabase 객체를 이용해서 더미데이터 인서트 테스트
+
     }
 }
